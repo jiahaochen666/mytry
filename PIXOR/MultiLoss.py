0dc8e198d15c8a7268d72bd38035e6c7991b5af6
jiahao <jiahaochen535@gmail.com>
2021-07-09 02:25:12 -0700

    change

--- PIXOR/MultiLoss.py
+++ PIXOR/MultiLoss.py
@@ -15,16 +15,14 @@ class MultiLoss(nn.Module):
         return loss.mean()
 
     def forward(self, predict, label):
-        cls_pre = predict[..., 0]
-        cls_lab = label[..., 0]
+        cls_pre = predict[..., -1]
+        cls_lab = label[..., -1]
         cls_loss = self.FocalLoss(cls_pre, cls_lab)
 
-        reg_pre = predict[..., 1:]
-        reg_lab = label[..., 1:]
+        reg_pre = predict[..., :-1]
+        reg_lab = label[..., :-1]
 
         smooth_l1 = nn.SmoothL1Loss(reduction='mean', beta=self.beta)
         reg_loss = smooth_l1(reg_pre, reg_lab)
-
         multi_loss = cls_loss.add(reg_loss)
-
         return multi_loss
